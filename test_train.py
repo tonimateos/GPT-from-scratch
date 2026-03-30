57cf4a736f893363f8b622db63bd1cb3104144eb
Toni <toni.mateos@gmail.com>
2026-03-30 17:42:20 +0200

    using a class

--- test_train.py
+++ test_train.py
@@ -1,7 +1,13 @@
-from train import *
+from train import Tokenizer
 
-def test_int_to_char():
-    chars = ['a', 'b', 'c']
-    assert int_to_char(2, chars) == 'c'
-    assert char_to_int('c', chars) == 2
+def test_int_and_char():
+    tokenizer = Tokenizer('abc')
+    assert tokenizer.int_to_char[2] == 'c'
+    assert tokenizer.char_to_int['c'] == 2
+
+def test_endode_decode():
+    tokenizer = Tokenizer('I wanted to say hello to you')
+    encoded_text = tokenizer.encode("hello")
+    decoded_text = tokenizer.decode(encoded_text)
+    assert decoded_text == "hello"
     
--- train.py
+++ train.py
@@ -1,33 +1,30 @@
 
+class Tokenizer:
+    def __init__(self, text):
+        self.chars = self.get_chars(text)
+        self.int_to_char = {i: c for i, c in enumerate(self.chars)}
+        self.char_to_int = {c: i for i, c in enumerate(self.chars)}
+
+    def get_chars(self, text):
+        return sorted(set(text))
+
+    def encode(self, text):
+        return [self.char_to_int[c] for c in text]
+    
+    def decode(self, encoded_text):
+        return "".join([self.int_to_char[i] for i in encoded_text])
+        
+    def printme(self):
+        print(self.int_to_char)
+        print(self.char_to_int)
+
+
 def read_training_set():
     with open("input.txt", 'r') as f:
         text = f.read()
     return text
 
-def get_chars(text):
-    return sorted(set(text))
-
-def int_to_char(integer, sorted_chars):
-    return sorted_chars[integer]
-
-def char_to_int(char, sorted_chars):
-    return sorted_chars.index(char)
-
-def encode(text, sorted_chars):
-    return [char_to_int(c, sorted_chars) for c in text]
-    
-def decode(encoded_text, sorted_chars):
-    return "".join([int_to_char(i, sorted_chars) for i in encoded_text])
-
-
 if __name__ == "__main__":
-    chars = get_chars(read_training_set())
-    int_for_c = char_to_int('c', chars)
-    char_for_int_for_c = int_to_char(int_for_c, chars)
-
-    print(f"The int for c is {int_for_c} and the char for {int_for_c} is {char_for_int_for_c}")
-    
-    encoded_text = encode("hello", chars)
-    print(encoded_text)
-    decoded_text = decode(encoded_text, chars)
-    print(decoded_text)
+    text = read_training_set()
+    tokenizer = Tokenizer(text)
+    tokenizer.printme()
