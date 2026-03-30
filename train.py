1b407a4f7ff18f71a45690b142b76cd3c28af357
Toni <toni.mateos@gmail.com>
2026-03-30 17:25:26 +0200

    adding encode decode

--- train.py
+++ train.py
@@ -13,6 +13,12 @@ def int_to_char(integer, sorted_chars):
 def char_to_int(char, sorted_chars):
     return sorted_chars.index(char)
 
+def encode(text, sorted_chars):
+    return [char_to_int(c, sorted_chars) for c in text]
+    
+def decode(encoded_text, sorted_chars):
+    return "".join([int_to_char(i, sorted_chars) for i in encoded_text])
+
 
 if __name__ == "__main__":
     chars = get_chars(read_training_set())
@@ -20,3 +26,8 @@ if __name__ == "__main__":
     char_for_int_for_c = int_to_char(int_for_c, chars)
 
     print(f"The int for c is {int_for_c} and the char for {int_for_c} is {char_for_int_for_c}")
+    
+    encoded_text = encode("hello", chars)
+    print(encoded_text)
+    decoded_text = decode(encoded_text, chars)
+    print(decoded_text)
